2e96938e1a9a59d5bf9f2f6b1e857a7818f87070
Aleksandr <47822964+titanq7@users.noreply.github.com>
2020-03-19 15:51:12 +0200

    min. fix for description

--- timer.js
+++ timer.js
@@ -1,59 +1,60 @@
 window.addEventListener("DOMContentLoaded", function() {
+  
   "use strick";
+  // Timer
 
-// Timer
+  let deadline = '2020-03-20'; // End date, deadline of our timer.
 
-let deadline = '2020-03-20'; // Конечная дата, deadline нашего таймера.
+  function getTimeRemaining (endtime){ // A function that calculates the remainder of the time and receives hours, minutes and seconds.
+    let t = Date.parse(endtime) - Date.parse(new Date()), 
+    seconds = Math.floor((t / 1000) % 60),  // We get seconds through the remainder. 
+    minutes =  Math.floor((t / 1000 / 60) % 60), // We get minutes.
+    hours = Math.floor((t / (1000*60*60))); // We get the minutes.
+   // days = Math.floor((t / (1000*60*60*24))); // Get the days.
 
-function getTimeRemaining (endtime){ // Функция которая вычисляет остаток времени и получает часы, минуты и секунды.
-  let t = Date.parse(endtime) - Date.parse(new Date()), 
-  seconds = Math.floor((t / 1000) % 60),  // Получаем секунды через остаток. 
-  minutes =  Math.floor((t / 1000 / 60) % 60), // получаем минуты.
-  hours = Math.floor((t / (1000*60*60))); // Получаем минуты.
- // days = Math.floor((t / (1000*60*60*24))); // Получаем дни.
-
- return { // Создаем обьект с получеными значениями и возращаем его. То есть getTimeRemaining(seconds), getTimeRemaining(minutes), getTimeRemaining(hours).
-   'total' : t,
-   'seconds' : seconds,
-   'minutes' : minutes,
-   'hours' : hours
- };
-}
-
-function setCloak(id, endtime) { // Создает различные переменные, 
-  let timer = document.getElementById(id), // Нужное ид будет помещаться в переменную timer.
-      seconds = timer.querySelector('.seconds'), // Кладем в переменные найденые классы через querySelector.
-      minutes = timer.querySelector('.minutes'), // Кладем в переменные найденые классы через querySelector.
-      hours = timer.querySelector('.hours'), // Кладем в переменные найденые классы через querySelector.
-      timeInterval = setInterval(updateClock, 1000); // Установили для фукнции updateClock интервал запуска каждую секунду.
-
-  function updateClock() { // Получает разницу между временем и записывает данные в классы.
-
-    let t = getTimeRemaining(endtime); // Кладем в t полученные данные из функции getTimeRemaining.
-    hours.textContent = t.hours; // записывааем в класс hours пролученные getTimeRemaining(hours).
-    minutes.textContent = t.minutes;
-    seconds.textContent = t.seconds;
-
-    function addZero() { // Если getTimeRemaining(свойство) < 10 добавляем '0' к t.seconds.
-      if (t.seconds < 10) {
-        seconds.textContent = '0' + t.seconds;
-      }
-      if (t.minutes < 10) {
-        minutes.textContent = '0' + t.minutes;
-      }
-      if (t.hours < 10) {
-        hours.textContent = '0' + t.hours;
-      }
-    }
-    addZero(); // Активируем функцию для добавления '0'.
-
-    if (t <= 0) {
-      clearInterval(timeInterval); // если t <= 0 , останавливаем метод timeInterval.
-    }
-
+   return { // We create an object with the obtained values and return it. getTimeRemaining(endtime).seconds, getTimeRemaining(endtime).seconds, getTimeRemaining(endtime).hours.
+     'total' : t,
+     'seconds' : seconds,
+     'minutes' : minutes,
+     'hours' : hours
+   };
   }
-}
 
-setCloak('timer', deadline); // id и конец отчета. deadline можно заменить на '2020-03-20' - обязательно тип данных строка.
+  function setCloak(id, endtime) { // Creates various variables, 
+    let timer = document.getElementById(id), // The desired id will be placed in the timer variable.
+        seconds = timer.querySelector('.seconds'), // We put the found classes in variables through querySelector.
+        minutes = timer.querySelector('.minutes'), // We put the found classes into variables through querySelector.
+        hours = timer.querySelector('.hours'), // We put the found classes in variables through querySelector.
+        timeInterval = setInterval(updateClock, 1000); // Set the updateClock function to the launch interval every second.
+
+    function updateClock() { // Gets the difference between time and writes data to classes.
+
+      let t = getTimeRemaining(endtime); // We put in t the received data from the getTimeRemaining function.
+      hours.textContent = t.hours; // write to class hours received getTimeRemaining(endtime).hours.
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+
+      function addZero() { // If getTimeRemaining (property) <10 add '0' to t.seconds.
+        if (t.seconds < 10) {
+          seconds.textContent = '0' + t.seconds;
+        }
+        if (t.minutes < 10) {
+          minutes.textContent = '0' + t.minutes;
+        }
+        if (t.hours < 10) {
+          hours.textContent = '0' + t.hours;
+        }
+      }
+      addZero(); // We activate the function to add '0'.
+
+      if (t <= 0) {
+        clearInterval(timeInterval); // if t <= 0, stop the timeInterval method.
+      }
+
+    }
+  }
+
+  setCloak('timer', deadline); // id and end of report. deadline can be replaced with '2020-03-20' - the string data type is required.
 
 });
